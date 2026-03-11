beaa2568f2f074f1f3e7f01b617561a1a92576fe
SwarzKalman <fekete.kalman@gmail.com>
2026-03-11 20:15:24 +0100

    alapweboldal

---
+++ locales/en.js
@@ -0,0 +1,22 @@
+window.voyagerLocales = window.voyagerLocales || {};
+window.voyagerLocales.en = {
+  htmlLang: "en",
+  pageTitle: "Voyager Maps",
+  title: "Voyager Maps",
+  intro: "The official Voyager Maps website where documentation and legal information are also available.",
+  legalAria: "Legal documents",
+  legalTitle: "Legal documents",
+  privacyLink: "Privacy Policy",
+  termsLink: "Terms of Services",
+  deleteLink: "Delete Data",
+  downloadAria: "App downloads",
+  downloadTitle: "Voyager app download",
+  downloadText: "Voyager Maps is available on Android and iOS.",
+  androidLabel: "Android (Google Play)",
+  iosLabel: "iOS (App Store)",
+  androidAria: "Voyager Maps Android download",
+  iosAria: "Voyager Maps iOS download",
+  androidQrLabel: "Android QR",
+  iosQrLabel: "iOS QR",
+  footer: "© Voyager Maps website"
+};
---
+++ locales/hu.js
@@ -0,0 +1,22 @@
+window.voyagerLocales = window.voyagerLocales || {};
+window.voyagerLocales.hu = {
+  htmlLang: "hu",
+  pageTitle: "Voyager Maps",
+  title: "Voyager Maps",
+  intro: "A Voyager Maps hivatalos weboldala, ahol a dokumentációk és jogi információk is elérhetők.",
+  legalAria: "Jogi dokumentumok",
+  legalTitle: "Jogi dokumentumok",
+  privacyLink: "Adatvédelmi tájékoztató",
+  termsLink: "Általános szerződési feltételek",
+  deleteLink: "Adattörlés",
+  downloadAria: "Alkalmazás letöltése",
+  downloadTitle: "Voyager app letöltés",
+  downloadText: "A Voyager Maps elérhető Android és iOS platformokon.",
+  androidLabel: "Android (Google Play)",
+  iosLabel: "iOS (App Store)",
+  androidAria: "Voyager Maps Android letöltés",
+  iosAria: "Voyager Maps iOS letöltés",
+  androidQrLabel: "Android QR",
+  iosQrLabel: "iOS QR",
+  footer: "© Voyager Maps weboldal"
+};
